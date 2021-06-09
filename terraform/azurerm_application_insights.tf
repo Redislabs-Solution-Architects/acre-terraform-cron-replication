5b4c22763bff8fcfd69923dba121e2fe15987b94
DaShaun <dashaun@redislabs.com>
2021-06-09 14:17:46 -0500

    update name

--- terraform/azurerm_application_insights.tf
+++ terraform/azurerm_application_insights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "primary" {
-  name                = random_string.app_insights_primary.result
+  name                = format("primary-%s", random_string.export_function_name.result)
   location            = random_shuffle.regions-with-az.result[0]
   resource_group_name = azurerm_resource_group.resource_group.name
   application_type    = "java"
