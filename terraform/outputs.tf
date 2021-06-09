35026065875426ae2ae5b6f3d35cde20642bbaa8
DaShaun <dashaun@redislabs.com>
2021-06-09 00:48:20 -0500

    add outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,22 +3,24 @@ locals {
     resource_group_name = azurerm_resource_group.resource_group.name
   }
   primary_info = {
-    id          = azurerm_redis_enterprise_cluster.primary.id
-    hostname    = azurerm_redis_enterprise_cluster.primary.hostname
-    access_key  = azurerm_redis_enterprise_database.primary.primary_access_key
-    database_id = azurerm_redis_enterprise_database.primary.id
-    storage_key = azurerm_storage_account.primary.primary_access_key
-    account_sas = data.azurerm_storage_account_sas.primary.sas
-    blob_sas = data.azurerm_storage_account_blob_container_sas.primary.sas
+    id           = azurerm_redis_enterprise_cluster.primary.id
+    hostname     = azurerm_redis_enterprise_cluster.primary.hostname
+    access_key   = azurerm_redis_enterprise_database.primary.primary_access_key
+    database_id  = azurerm_redis_enterprise_database.primary.id
+    storage_name = azurerm_storage_account.primary.name
+    storage_key  = azurerm_storage_account.primary.primary_access_key
+    account_sas  = data.azurerm_storage_account_sas.primary.sas
+    blob_sas     = data.azurerm_storage_account_blob_container_sas.primary.sas
   }
   secondary_info = {
     id          = azurerm_redis_enterprise_cluster.secondary.id
     hostname    = azurerm_redis_enterprise_cluster.secondary.hostname
     access_key  = azurerm_redis_enterprise_database.secondary.primary_access_key
     database_id = azurerm_redis_enterprise_database.secondary.id
+    storage_name = azurerm_storage_account.secondary.name
     storage_key = azurerm_storage_account.secondary.primary_access_key
     account_sas = data.azurerm_storage_account_sas.secondary.sas
-    blob_sas = data.azurerm_storage_account_blob_container_sas.secondary.sas
+    blob_sas    = data.azurerm_storage_account_blob_container_sas.secondary.sas
   }
 }
 
