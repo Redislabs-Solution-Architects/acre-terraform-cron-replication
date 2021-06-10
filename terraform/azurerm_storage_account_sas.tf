404c4cb6fe744769e6ee3667cfcbef2de178732a
DaShaun <dashaun@redislabs.com>
2021-06-10 18:01:51 -0500

    function uses different type of sas for deployment

---
+++ terraform/azurerm_storage_account_sas.tf
@@ -0,0 +1,27 @@
+data "azurerm_storage_account_sas" "sas" {
+  connection_string = azurerm_storage_account.primary.primary_connection_string
+  https_only        = true
+  start             = "2021-06-01"
+  expiry            = "2022-06-30"
+  resource_types {
+    object    = true
+    container = false
+    service   = false
+  }
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+  permissions {
+    read    = true
+    write   = false
+    delete  = false
+    list    = false
+    add     = false
+    create  = false
+    update  = false
+    process = false
+  }
+}
