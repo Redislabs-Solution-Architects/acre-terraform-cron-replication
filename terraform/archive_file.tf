112c2477edb875b636ea8c4e4ec568f2667779bd
DaShaun <dashaun@redislabs.com>
2021-06-21 21:48:32 -0500

    archive file for import function app

--- terraform/archive_file.tf
+++ terraform/archive_file.tf
@@ -6,6 +6,12 @@ data "archive_file" "export" {
 
 data "archive_file" "copy" {
   type        = "zip"
+  source_dir  = "../com.redisgeek.function.acre.import/target/azure-functions/azure-import-function/"
+  output_path = "import-function.zip"
+}
+
+data "archive_file" "copy" {
+  type        = "zip"
   source_dir  = "../com.redisgeek.function.azure.storage.copy/target/azure-functions/azure-storage-copy-function/"
   output_path = "copy-function.zip"
 }
