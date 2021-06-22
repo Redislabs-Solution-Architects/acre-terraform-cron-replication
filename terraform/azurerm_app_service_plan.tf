1f3800f6a8af80e14c6962178e4d429bb60d2f2b
DaShaun <dashaun@redislabs.com>
2021-06-22 05:29:17 -0500

    Add secondary app service plan

--- terraform/azurerm_app_service_plan.tf
+++ terraform/azurerm_app_service_plan.tf
@@ -9,3 +9,15 @@ resource "azurerm_app_service_plan" "primary_plan" {
     size = "Y1"
   }
 }
+
+resource "azurerm_app_service_plan" "secondary_plan" {
+  name                = "secondary-plan"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = random_shuffle.regions-with-az.result[1]
+  kind                = "functionapp"
+  reserved            = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
