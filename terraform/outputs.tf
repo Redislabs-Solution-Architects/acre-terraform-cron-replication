a00ac63f64565e3851b1f88548bd3cd3d4368bd6
DaShaun <dashaun@redislabs.com>
2021-06-22 05:36:00 -0500

    add outputs and additional random strings for unique

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,20 +3,29 @@ locals {
     resource_group_name = azurerm_resource_group.resource_group.name
   }
   primary_info = {
-    id                = azurerm_redis_enterprise_cluster.primary.id
-    hostname          = azurerm_redis_enterprise_cluster.primary.hostname
-    access_key        = azurerm_redis_enterprise_database.primary.primary_access_key
-    database_id       = azurerm_redis_enterprise_database.primary.id
-    storage_account   = azurerm_storage_account.primary.name
-    storage_container = azurerm_storage_container.primary.name
+    id                        = azurerm_redis_enterprise_cluster.primary.id
+    hostname                  = azurerm_redis_enterprise_cluster.primary.hostname
+    access_key                = azurerm_redis_enterprise_database.primary.primary_access_key
+    database_id               = azurerm_redis_enterprise_database.primary.id
+    storage_account           = azurerm_storage_account.primary.name
+    storage_account_endpoint  = azurerm_storage_account.primary.primary_blob_endpoint
+    storage_container         = azurerm_storage_container.primary.name
+    storage_connection_string = azurerm_storage_account.primary.primary_blob_connection_string
+    storage_key               = azurerm_storage_account.primary.primary_access_key
+    blob_sas                  = data.azurerm_storage_account_blob_container_sas.primary.sas
   }
   secondary_info = {
-    id                = azurerm_redis_enterprise_cluster.secondary.id
-    hostname          = azurerm_redis_enterprise_cluster.secondary.hostname
-    access_key        = azurerm_redis_enterprise_database.secondary.primary_access_key
-    database_id       = azurerm_redis_enterprise_database.secondary.id
-    storage_account   = azurerm_storage_account.secondary.name
-    storage_container = azurerm_storage_container.secondary.name
+    account_sas               = data.azurerm_storage_account_sas.primary.sas
+    id                        = azurerm_redis_enterprise_cluster.secondary.id
+    hostname                  = azurerm_redis_enterprise_cluster.secondary.hostname
+    access_key                = azurerm_redis_enterprise_database.secondary.primary_access_key
+    database_id               = azurerm_redis_enterprise_database.secondary.id
+    storage_account           = azurerm_storage_account.secondary.name
+    storage_account_endpoint  = azurerm_storage_account.secondary.primary_blob_endpoint
+    storage_container         = azurerm_storage_container.secondary.name
+    storage_connection_string = azurerm_storage_account.secondary.primary_blob_connection_string
+    storage_key               = azurerm_storage_account.secondary.primary_access_key
+    blob_sas                  = data.azurerm_storage_account_blob_container_sas.secondary.sas
   }
 }
 
--- terraform/random_string.tf
+++ terraform/random_string.tf
@@ -75,7 +75,21 @@ resource "random_string" "app_insights_secondary" {
   upper   = false
 }
 
+resource "random_string" "copy_function_name" {
+  length  = 12
+  number  = false
+  special = false
+  upper   = false
+}
+
 resource "random_string" "export_function_name" {
+  length  = 12
+  number  = false
+  special = false
+  upper   = false
+}
+
+resource "random_string" "import_function_name" {
   length  = 12
   number  = false
   special = false
